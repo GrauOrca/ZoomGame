9f076c60eb2fe5c62dce352fd409efa463c94021
Galm - G.Laptop <sebastian.rodriguez.lema@gmail.com>
2017-12-16 02:14:01 -0500

    Camera Changes

--- Assets/Scritps/CameraMovement.cs
+++ Assets/Scritps/CameraMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-
+	public float maxFOV = 160;
+	public float minFOV = 0;
 	// Use this for initialization
 	void Start () 
 	{
@@ -15,10 +16,12 @@ public class CameraMovement : MonoBehaviour {
 	void Update () 
 	{
 		var d = Input.GetAxis ("Mouse ScrollWheel");
-		if (d > 0f)
-			Camera.main.fieldOfView = Camera.main.fieldOfView + 1;
-		else if (d < 0f)
-			Camera.main.fieldOfView = Camera.main.fieldOfView - 1;
+		if ((d > 0f) && ( Camera.main.fieldOfView < maxFOV))
+			Camera.main.fieldOfView = Camera.main.fieldOfView + 10;
+			// Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, Camera.main.fieldOfView + 10, 2*Time.deltaTime);
+		else if ((d < 0f) && (Camera.main.fieldOfView > minFOV))
+			Camera.main.fieldOfView = Camera.main.fieldOfView - 10;
+			// Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, Camera.main.fieldOfView - 10, 2*Time.deltaTime);
 
 		if (Input.GetKeyDown("d"))
 			transform.position =  new Vector3(transform.position.x + 1,transform.position.y, transform.position.z);
