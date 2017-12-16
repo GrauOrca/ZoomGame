cf5b9a20febc2716234ae770d730a1780d51a720
Gabriel Ramirez <g_e_ramirez@outlook.com>
2017-12-16 07:52:30 -0500

    sada

---
+++ Assets/Scritps/Laser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTrigger2DEnter(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("There's a Player in the road");
+		}
+	}
+
+}
---
+++ Assets/Scritps/LaserIO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserIO : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnBecameVisible() {
+		Debug.Log ("I can be seen");
+	}
+}
