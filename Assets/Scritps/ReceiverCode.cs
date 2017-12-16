96fe40481b04029c33f3bdb82558ebe046aecd45
Galm - G.Laptop <sebastian.rodriguez.lema@gmail.com>
2017-12-16 08:18:14 -0500

    Bridge - RigidBody + Trigger

--- Assets/Scritps/ReceiverCode.cs
+++ Assets/Scritps/ReceiverCode.cs
@@ -19,12 +19,12 @@ public class ReceiverCode : MonoBehaviour {
 	{
 		// **Block Receives TRUE information**
 		if (detectionCode.detector == true) // Uses the detector variable from the Detection Code script
-			GetComponent<SpriteRenderer> ().color = Color.black;
+			GetComponent<Rigidbody2D> ().simulated = false;
 			Debug.Log ("Bridge Open");
 
 		// **Block Receives FALSE information from **
 		if (detectionCode.detector == false)
-			GetComponent<SpriteRenderer> ().color = Color.white;
+			GetComponent<Rigidbody2D> ().simulated = true;
 			Debug.Log ("No bridge for you");
 
 	}
