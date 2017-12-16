b768e649ca1b500eb4e6ab2223eeceb2b0aa2829
Gabriel Ramirez <g_e_ramirez@outlook.com>
2017-12-16 02:33:30 -0500

    lalala

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5;
+	Rigidbody2D rigi;
+	public Vector2 Horizontal;
+	public Vector2 Vertical;
+	// Use this for initialization
+	void Start () {
+		rigi = transform.GetComponent<Rigidbody2D> ();
+	}
+
+	void movement(){
+		if (Input.GetKey (KeyCode.W)) {
+			rigi.MovePosition (new Vector2(transform.position.x,transform.position.y  + speed));
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rigi.MovePosition (new Vector2(transform.position.x,transform.position.y  - speed));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rigi.MovePosition (new Vector2(transform.position.x - speed,transform.position.y));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rigi.MovePosition (new Vector2(transform.position.x + speed,transform.position.y));
+
+		}
+	}		
+	// Update is called once per frame
+	void Update(){
+		movement ();
+	}
+}
